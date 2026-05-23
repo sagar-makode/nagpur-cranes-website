9276f0415f274ba115d50168b7306c565dc5258c
myexamworld <myexamworld1@gmail.com>
2026-05-23 19:20:11 +0530

    image fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Enables simple deployments on standard platforms like Netlify/Vercel without configuring advanced image loaders.
+    qualities: [75, 90, 100],
   },
 };
 
